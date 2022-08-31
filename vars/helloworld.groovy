9f51afc60bc4cea9351c3cd0c29e00cb8a98762b
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-31 10:37:16 -0600

    Llamar al script desde resource

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,3 +1,9 @@
-def call(Map config = [:]){
-    sh "echo hey  como estas ${config.name}. Hoy es ${config.day}?"
-}// prueba d
+// def call(Map config = [:]){
+//     sh "echo hey  como estas ${config.name}. Hoy es ${config.day}?"
+// }// prueba d
+
+//HelloWorld External
+def call(Map config = [:]) {
+    loadLinuxScript(name: 'hello-world.sh')
+    sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+}
