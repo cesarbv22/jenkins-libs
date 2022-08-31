c940b24ea9c6c5f23f839f399f3d436a934ba61d
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-31 14:53:17 -0600

    prueba de stage checkout

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]){
   // checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
- checkout([$class: ${config.class}, branches: [[name: ${config.name}]], extensions: [], userRemoteConfigs: [[url: ${config.url_repo}]]])
+ checkout([$class: '${config.class}', branches: [[name: '${config.name}']], extensions: [], userRemoteConfigs: [[url: '${config.url_repo}']]])
 
 }
