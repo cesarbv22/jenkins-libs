1bc9b3e95c702ac11f1369cd0f4fff7b0b3cc11b
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-31 14:48:44 -0600

    Prueba de parametros al stage de checkout

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,3 +1,5 @@
-def call (){
-   checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
+def call (Map config = [:]){
+  // checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
+ checkout([$class: ${config.class}, branches: [[name: ${config.name}]], extensions: [], userRemoteConfigs: [[url: ${config.url_repo}]]])
+
 }
