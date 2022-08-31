93f2ff3dd06189df55563a65de76a57bc6c490ce
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-31 15:04:44 -0600

    adads

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,5 +1,3 @@
-//def call (Map config = [:]){
 def call (){
-  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
- //checkout([$class: 'GitSCM', branches: [[name: '${config.name}']], extensions: [], userRemoteConfigs: [[url: '${config.url_repo}']]])
+   checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
 }
