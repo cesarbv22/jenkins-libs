f3585b9b5ea5b2d6ac15a3f890458ecdb478782d
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-31 11:53:12 -0600

    nuevo load para ver si funca

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,4 +1,4 @@
 def call (){
-     loadLinuxScript(name: 'hello.sh') //Llamo al comando que ocupo para ejecutar el recurso de echo
+     helloLoadScript(name: 'hello.sh') //Llamo al comando que ocupo para ejecutar el recurso de echo
      sh "./hello.sh" // por lo que entiendo este es el intérprete de comandos
 }
