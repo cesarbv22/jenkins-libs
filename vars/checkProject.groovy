8a2b8cb690cb664ffce81736ce6a15b5384be5d6
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-01 14:53:06 -0600

    delete map config in checkProject Groovy

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,4 +1,5 @@
-def call (Map config = [:]){ //quitar ese map si no lo uso
+//def call (Map config = [:]){ //quitar ese map si no lo uso
+def call (){ //quitar ese map si no lo uso
     checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
    //checkout([$class: ${config.type}, branches: [[name: ${config.branchs}]], extensions: [], userRemoteConfigs: [[url: ${config.repo}]]])
 }
