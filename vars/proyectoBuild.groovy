749cdb464d4a4e239c05cbafa024ca6148b34e49
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-01 08:23:38 -0600

    prueba build groovy

--- vars/proyectoBuild.groovy
+++ vars/proyectoBuild.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]){
-     loadLinuxScript(name: 'project-build.sh')
-     sh "./project-build.sh ${config.project_root} ${config.registry} ${config.build_number}" 
+    // loadLinuxScript(name: 'project-build.sh')
+    // sh "./project-build.sh ${config.project_root} ${config.registry} ${config.build_number}" 
+    sh "cd ./${config.project_root};docker build -t ${config.registry}:${config.BUILD_NUMBER} . "
 }
