a1fa77d0aadc9f6933aca1aa701cd610a00cefdc
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-01 14:41:13 -0600

    Retroceso

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,4 +1,4 @@
 def call (Map config = [:]){ //quitar ese map si no lo uso
-   // checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
-   checkout([$class: ${config.type}, branches: [[name: ${config.branchs}]], extensions: [], userRemoteConfigs: [[url: ${config.repo}]]])
+    checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
+   //checkout([$class: ${config.type}, branches: [[name: ${config.branchs}]], extensions: [], userRemoteConfigs: [[url: ${config.repo}]]])
 }
