2a190f05c8b554fbbcad3993b410bd898e669d23
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-01 09:10:09 -0600

    Prueba install

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,3 +1,3 @@
-def call (Map config = [:]){
+def call (Map config = [:]){ //quitar ese map si no lo uso
    checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
 }
--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
-    // loadLinuxScript(name: 'dependencies-install.sh')
-    // sh "./dependencies-install.sh ${config.project_root}" 
-    sh 'npm --version'
-    sh "cd ${config.project_root}; npm install" 
+    loadLinuxScript(name: 'dependencies-install.sh')
+    sh "./dependencies-install.sh ${config.project_root}" 
+    // sh 'npm --version'
+    // sh "cd ${config.project_root}; npm install" 
 }
