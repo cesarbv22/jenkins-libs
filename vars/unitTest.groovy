cdb861e5af7c37426a0ff7f6bdead83c3103bb98
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-01 09:30:13 -0600

    Sirva mierda

--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    loadLinuxScript(name: 'unit-test.sh')
-    sh "./unit-test.sh ${config.project_root}"
-    //sh "cd ${config.project_root}; npm run test"
+    // loadLinuxScript(name: 'unit-test.sh')
+    // sh "./unit-test.sh ${config.project_root}"
+    sh "cd ${config.project_root}; npm run test"
 }
