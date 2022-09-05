894c9492464721549d66b9244fedf6482daa3911
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-05 11:45:02 -0600

    dadadad

--- vars/checkProject.groovy
+++ vars/checkProject.groovy
@@ -1,6 +1,6 @@
 def call (Map config = [:]){ //quitar ese map si no lo uso
 //def call (){ //quitar ese map si no lo uso
     //checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/cesarbv22/docker-pirate']]])
-   sh 'echo prueba Parametros checkout'
-   checkout([$class: ${config.type}, branches: [[name: ${config.branchs}]], extensions: [], userRemoteConfigs: [[url: ${config.repo}]]])
+   sh 'echo prueba Parametros checkout ${config.type} ${config.branchs} ${config.repo} '
+  // checkout([$class: ${config.type}, branches: [[name: ${config.branchs}]], extensions: [], userRemoteConfigs: [[url: ${config.repo}]]])
 }
