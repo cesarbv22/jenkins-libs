414e080103e73fedea02c8c14bf940b72d94ba56
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-05 11:11:56 -0600

    Prueba con las credenciales

--- vars/proyectBuild.groovy
+++ vars/proyectBuild.groovy
@@ -2,6 +2,7 @@ def call(Map config = [:]){
     // loadLinuxScript(name: 'project-build.sh')
     // sh "./project-build.sh ${config.project_root} ${config.registry} ${config.build_number}" 
     //sh "cd ./${config.project_root};docker build -t ${config.registry}:${config.BUILD_NUMBER} . "
-     sh "docker login"
+    // sh "docker login"
+     sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
      sh "cd ./${config.project_root};docker build -t ${config.registry}:${config.buildNumber} . "
 }
