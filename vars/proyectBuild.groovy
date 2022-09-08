ec37a652b4c410bfc1b299fff3ec5b6d9d259597
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-08 10:44:25 -0600

    corregir credenciales

--- vars/proyectBuild.groovy
+++ vars/proyectBuild.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){
-    // sh "docker login"
-     sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+     sh "docker login"
+    // sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
      sh "cd ./${config.project_root};docker build -t ${config.registry}:${config.buildNumber} . "
 }
