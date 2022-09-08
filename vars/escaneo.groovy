a2d14aa6d28e4fa59bb351d2dfdc20768970a2ab
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-08 10:07:28 -0600

    Prueba compose pipeline

--- vars/escaneo.groovy
+++ vars/escaneo.groovy
@@ -1,12 +1,26 @@
+// def call(Map config = [:]){
+//      sh "${config.homeScanner}/bin/sonar-scanner \
+//                            -Dsonar.projectKey=SimpleExpressExample:Test \
+//                            -Dsonar.projectName=SimpleExpressExample \
+//                            -Dsonar.projectVersion=0.0.${config.buildNumber} \
+//                            -Dsonar.host.url=http://mysonarqube:9000 \
+//                            -Dsonar.sources=./${config.project_root}/app.js,./${config.project_root}/config/db.config.js,./${config.project_root}/routes/developers.js \
+//                            -Dsonar.login=admin \
+//                            -Dsonar.password=admin \
+//                            -Dsonar.tests=./${config.project_root}/test \
+//                            -Dsonar.javascript.lcov.reportPaths=./${config.project_root}/coverage/lcov.info"
+// }
+
+
 def call(Map config = [:]){
      sh "${config.homeScanner}/bin/sonar-scanner \
                            -Dsonar.projectKey=SimpleExpressExample:Test \
                            -Dsonar.projectName=SimpleExpressExample \
                            -Dsonar.projectVersion=0.0.${config.buildNumber} \
-                           -Dsonar.host.url=http://mysonarqube:9000 \
+                           -Dsonar.host.url=http://mysonarqube:8888 \
                            -Dsonar.sources=./${config.project_root}/app.js,./${config.project_root}/config/db.config.js,./${config.project_root}/routes/developers.js \
                            -Dsonar.login=admin \
-                           -Dsonar.password=admin \
+                           -Dsonar.password=Zhasam8080 \
                            -Dsonar.tests=./${config.project_root}/test \
                            -Dsonar.javascript.lcov.reportPaths=./${config.project_root}/coverage/lcov.info"
 }
