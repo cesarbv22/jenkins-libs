58f99b67d61c3847833b6a180a3d531c3e4a6a28
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-12 10:34:14 -0600

    iii

--- vars/createContainer.groovy
+++ vars/createContainer.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]) {
     //sh "docker run -it -d -p 3001:3000 --name plTest cesarbv/docker-pirate-express:46"
     //sh "docker run -it -d -p ${config.port}:3000 --name ${config.name} ${config.registry}:${config.buildNumber}"
-    sh "docker run --rm -p 3001:3000 --name ${config.name} ${config.registry}:51"
+    sh "docker run -i --rm -p 3001:3000 --name ${config.name} ${config.registry}:51"
 }
