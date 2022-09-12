33a138a7802e10fae6045b7ec98fb0a7567ee9c1
cesarbv22 <cesar.boza@grupobabel.com>
2022-09-12 10:00:29 -0600

    Prueba de crear container en premisa

---
+++ vars/createContainer.groovy
@@ -0,0 +1,4 @@
+def call (Map config = [:]) {
+    //sh "docker run -it -d -p 3001:3000 --name plTest cesarbv/docker-pirate-express:46"
+    sh "docker run -it -d -p ${config.port}:3000 --name ${config.name} ${config.registry}:${config.buildNumber}"
+}
